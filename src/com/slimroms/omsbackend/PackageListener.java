3d67e483467ad469e5d627c65631206a736c834d
Griffin Millender <griffinn.millender@gmail.com>
2017-06-10 12:46:48 -0500

    OmsBackend: Add a package listener to handle unsupported themes
    Change-Id: I9f5a1bbe3534df6456b7f858d0f37b60769aa8bf

---
+++ src/com/slimroms/omsbackend/PackageListener.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2017 SlimRoms Project
+ * Copyright (C) 2017 Victor Lapin
+ * Copyright (C) 2017 Griffin Millender
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.slimroms.omsbackend;
+
+import android.app.AlertDialog;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.text.TextUtils;
+import android.util.Log;
+
+public class PackageListener extends BroadcastReceiver {
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+
+        String action = intent.getAction();
+        if (TextUtils.isEmpty(action)) return;
+        if (intent.getData() == null) return;
+        if (!intent.getData().getScheme().equals("package")) return;
+
+        final String packageName = intent.getData().getSchemeSpecificPart();
+        boolean replacing = intent.getBooleanExtra(Intent.EXTRA_REPLACING, false);
+
+        // check for unsupported theme
+        if (action.equals(Intent.ACTION_PACKAGE_ADDED)) {
+            try {
+                ApplicationInfo aInfo = context.getPackageManager().getApplicationInfo(packageName,
+                        PackageManager.GET_META_DATA);
+                if (aInfo.metaData != null && aInfo.metaData.containsKey("Substratum_Name")) {
+                    boolean supportsThirdParty =
+                            aInfo.metaData.getBoolean("supports_third_party_theme_systems", false);
+                    if (!supportsThirdParty) {
+                        String message = String.format(context.getString(
+                                R.string.theme_unsupported_install_notification),
+                                aInfo.metaData.getString("Substratum_Name"));
+
+                        Intent showIntent = new Intent();
+                        PendingIntent contentIntent =
+                                PendingIntent.getActivity(context, 0, showIntent, 0);
+
+                        Notification.Builder notifBuilder = new Notification.Builder(context);
+                        notifBuilder.setContentIntent(contentIntent);
+                        notifBuilder.setContentTitle(context.getString(
+                                R.string.theme_unsupported_install_notification_title));
+                        notifBuilder.setContentText(message);
+                        notifBuilder.setSmallIcon(R.drawable.delete);
+                        notifBuilder.setPriority(Notification.PRIORITY_MAX);
+                        notifBuilder.setVibrate(new long[] { 500 });
+                        context.getSystemService(NotificationManager.class)
+                                .notify(101, notifBuilder.build());
+
+                        Intent activity = new Intent(context, UnsupportedThemeDialogActivity.class);
+                        activity.putExtra("theme_name", aInfo.metaData.getString("Substratum_Name"));
+                        activity.putExtra("package_name", packageName);
+                        context.startActivity(activity);
+                    }
+                }
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
